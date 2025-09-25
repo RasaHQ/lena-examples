101ab4e52737da3ba13a32cc5d6879566bd2d87e
Lena Nguyen <l.nguyen@rasa.com>
2025-09-25 15:23:21 +0100

    initial commit

---
+++ actions/actions.Dockerfile
@@ -0,0 +1,23 @@
+# specify the version of rasa-sdk image
+ARG VERSION=3.12.1
+
+# start from rasa-sdk image
+FROM rasa/rasa-sdk:${VERSION}
+
+# Copy the actions folder, endpoints.yml, db json files
+COPY ./actions /app/actions
+# COPY ./endpoints.yml /app/endpoints.yml
+COPY ./db /app/db
+
+
+USER root
+# install required packages
+RUN pip install -U pip
+RUN pip install --no-cache-dir -r /app/actions/requirements.txt
+
+# Expose the port the app runs on
+EXPOSE 5055
+
+# Command to run when the container starts
+USER 1001
+CMD ["start", "--actions", "actions"]
---
+++ actions/list_contacts.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, List, Text
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+from actions.db import get_contacts
+
+import logging
+logger = logging.getLogger(__name__)
+
+class ListContacts(Action):
+    def name(self) -> str:
+        return "list_contacts"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[Text, Any]]:
+        # Add test flag to trigger exception
+        test_error = tracker.get_slot("test_error")
+        if test_error:
+            dispatcher.utter_message(response="utter_internal_error_rasa")
+            logger.error("Test exception in list_contacts")
+            return [SlotSet("action_server_error", True)]
+
+        try:
+            contacts = get_contacts(tracker.sender_id)
+            if len(contacts) > 0:
+                contacts_list = "".join([f"- {c.name} ({c.handle}) \n" for c in contacts])
+                return [SlotSet("contacts_list", contacts_list)]
+            else:
+                return [SlotSet("contacts_list", None)]
+        except Exception as e:
+            dispatcher.utter_message(response="utter_internal_error_rasa")
+            return [SlotSet("contacts_list", None), SlotSet("action_server_error", True)]
